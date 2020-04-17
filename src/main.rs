d179f975b31fa5b8e452134e2a338d4d3790682c
Marco 'don' Kaulea <don@0xbeef.org>
2020-04-17 20:57:19 +0200

    Use strongly typed serde

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,70 @@
+use serde::{Serialize, Deserialize};
 use scraper::{Html, Selector};
 use serde_json::{Value};
+use std::io::BufReader;
+use std::fs::File;
+use std::collections::HashMap;
 
 const URI: &str = "https://www.dhl.de/int-verfolgen/search";
 
+#[derive(Serialize, Deserialize, Debug)]
+struct DHLContent {
+    #[serde(alias = "sendungen")]
+    shipments: Vec<Shipment>,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Shipment {
+    #[serde(alias = "hasCompleteDetails")]
+    has_complete_details: bool,
+    id: String,
+    #[serde(alias = "sendungsdetails")]
+    shipment_details: ShipmentDetails,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ShipmentDetails {
+    #[serde(alias = "istZugestellt")]
+    is_delivered : bool,
+    #[serde(alias = "eigenschaften")]
+    properties: HashMap<String, String>,
+    #[serde(alias = "sendungsverlauf")]
+    shipment_history: ShipmentHistory,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ShipmentHistory {
+    #[serde(alias = "datumAktuellerStatus")]
+    date_of_current_status: String,
+    #[serde(alias = "aktuellerStatus")]
+    current_status: String,
+    #[serde(alias = "kurzStatus")]
+    short_status: String,
+    events: Vec<Event>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Event {
+    #[serde(alias = "datum")]
+    date: String,
+    #[serde(alias = "ort")]
+    location: Option<String>,
+    #[serde(alias = "status")]
+    status: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("./state.json")?;
+    let reader = BufReader::new(file);
+    let parsed: DHLContent = serde_json::from_reader(reader)?;
+    println!("{}", serde_json::to_string_pretty(&parsed).unwrap());
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn _main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args();
     args.next();
     let id: &str = &args.next().unwrap();
